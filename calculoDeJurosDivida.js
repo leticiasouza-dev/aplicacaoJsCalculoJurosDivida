a8901d0a5e3e16c6bda0d7b0b036679974ec7738
leticiaSouzaDev <lec667121@gmail.com>
2023-10-16 17:54:04 -0300

    aplicação de calculo de juros de divida

--- calculoDeJurosDivida.js
+++ calculoDeJurosDivida.js
@@ -4,29 +4,39 @@ console.log("Aplicação de Juros:")
 
 //se houver mais de 15 dias de atrasa, a taxa de juros é de 10% do valor da divída, caso contrário, a ataxa é de 5% do valor da dívida
 
-let valorDevido = Number(input.question("Informe o valor devido: "))
+let valorDevido = Number(input.question("Informe o valor devido: "));
 
+if(valorDevido <= 0){
+    console.log("Aplicação encerrada");
 
-switch(valorDevido){
-    case valorDevido <= 0:
-         console.log("Sistema encerrado!!")
-         break
-
-    case valorDevido > 0:
-        let diasDeAtrasos = Number(input.question("Informe quantos dias se passaram desde o vencimento do boleto (dias de atraso): "))
-
+} else{
+    let diasDeAtrasos = Number(input.question("Informe os dias de atraso"));
+    let juros
+    
+    
+    
         if(diasDeAtrasos <= 0){
-            console.log("Sistema encerrado!")
-        } else if(diasDeAtrasos > 15){
-            juros 10%
-        } else{
-            juros 5%
+            console.log("Aplicação encerrada")
+        } else {
+            if(diasDeAtrasos > 15){
+                juros = 10;
+            } else{
+                juros = 5;
+            }
+
+            let valorJuros = (valorDevido / 100) * juros;
+            let totalDivida = valorDevido + valorJuros
+
+            console.log("Valor original da dívida: " + valorDevido);
+            console.log("Dias atrasados: " + diasDeAtrasos);
+            console.log("taxas de Juros: " + juros + "%");
+            console.log("Valor total com juros: " + totalDivida)
         }
 }
 
 
 
-console.log("Valor original da dívida: " + )
-console.log("Dias atrasados: " + )
-console.log("taxas de Juros: " + )
-console.log("Valor total com juros: " + )
+
+
+
+
